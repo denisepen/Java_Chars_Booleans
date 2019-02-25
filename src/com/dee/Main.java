274dd23128c94822174b9983ce331ca17b45710a
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2019-02-25 11:52:21 -0500

    first commit

---
+++ src/com/dee/Main.java
@@ -0,0 +1,16 @@
+package com.dee;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        char is 2 bytes - width of 16
+//	char myChar = '\u00A9';   unicode character
+
+        char myChar = 'd';
+        boolean myBoolean = true;
+
+        System.out.println(myBoolean);
+
+    }
+}
